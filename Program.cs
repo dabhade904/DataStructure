849991fec6a395a0aa9f6dc66d280d7517dcee40
dabhade904 <dabhade904@gmail.com>
2022-08-19 23:38:58 +0530

    Added : Prime number program

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace DataStructure
+{
+
+    public class PrimeArray2D
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList[] list = new ArrayList[10];
+            int count = 0, i = 0, temp = 100;
+
+            for (int j = 0; j < 10; j++)
+            {
+                list[j] = new ArrayList();
+            }
+
+            while (i < 10)
+            {
+                for (int num = 2; num <= 1000; num++)
+                {
+                    count = Utility.isPrime(num);
+
+                    if (count == 0)
+                    {
+                        list[i].Add(num);
+                    }
+                    if (num == temp)
+                    {
+                        i++;
+                        temp = temp + 100;
+                    }
+                }
+
+            }
+            /*  for (int j = 0; j < list.Length; j++)
+              {
+                  Console.WriteLine(list[j]);
+              }*/
+            foreach (var li in list)
+            {
+                Console.WriteLine("" + li);
+            }
+        }
+
+
+    }
+
+}
---
+++ Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Utility
+    {
+        public static int isPrime(int num)
+        {
+            int cnt = 0;
+            for (int i = 2; i < num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    cnt++;
+                    break;
+                }
+            }
+            return cnt;
+        }
+
+    }
+}
